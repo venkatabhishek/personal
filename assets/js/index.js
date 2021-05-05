d038c80c39f6c6b80c0d092ca91573d5c7c3b4b5
Abhishek Venkat <abhisaivenkat@gmail.com>
2021-05-05 16:58:04 -0400

    re-add animations

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// console.log(Math.random());
+
+
+// window.onload = function(){
+//     anime({
+//         targets: ".hello-txt",
+//         color: "#000",
+//         duration: 3000
+//     })
+// }
